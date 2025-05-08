4c52da38c20d442a0bbd444f29c67a4a8927dd2e
nahye <knh8783@gmail.com>
2025-05-08 11:03:22 +0900

    add User entity and repository and controller

---
+++ src/main/java/com/basic/myspringboot/controller/UserRestController.java
@@ -0,0 +1,36 @@
+package com.basic.myspringboot.controller;
+
+import com.basic.myspringboot.entity.User;
+import com.basic.myspringboot.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+//@Controller + @ResponseBody
+@RestController
+@RequiredArgsConstructor
+//final 인 변수를 초기화하는 생성자를 자동으로 생성해주는 역할을 하는 롬복 어노테이션
+@RequestMapping("/api/users")
+public class UserRestController {
+    private final UserRepository userRepository;
+
+    //Constructor Injection
+//    public UserRestController(UserRepository userRepository) {
+//        System.out.println(">>> UserController " + userRepository.getClass().getName());
+//        this.userRepository = userRepository;
+//    }
+
+    @PostMapping
+    public User create(@RequestBody User user) {
+        return userRepository.save(user);
+    }
+
+    @GetMapping
+    public List<User> getUsers() {
+        return userRepository.findAll();
+    }
+
+
+
+}
