fd86917fafd532dc6e6e5bc17071c9dcee1bff5a
nahye <knh8783@gmail.com>
2025-05-09 03:56:51 +0900

    modify

--- src/main/java/com/basic/myspringboot/entity/Book.java
+++ src/main/java/com/basic/myspringboot/entity/Book.java
@@ -31,6 +31,6 @@ public class Book {
     @CreationTimestamp
     private LocalDate publishDate = LocalDate.now();
 
-    @Column(unique = true, nullable = false)
+    @Column(nullable = false)
     private int price;
 }
