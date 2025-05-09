8ba90dcdc7c71431e616730efbcd82a496caf74e
nahye <knh8783@gmail.com>
2025-05-09 17:01:45 +0900

    modify

---
+++ src/main/java/com/basic/myspringboot/controller/UserServiceController.java
@@ -0,0 +1,52 @@
+package com.basic.myspringboot.controller;
+
+import com.basic.myspringboot.controller.dto.UserDTO;
+import com.basic.myspringboot.entity.User;
+import com.basic.myspringboot.service.UserService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/users")
+public class UserServiceController {
+    private final UserService userService;
+
+    @PostMapping
+    public UserDTO.UserResponse create(@Valid @RequestBody
+                                       UserDTO.UserCreateRequest request) {
+        User user = new User();
+        user.setName(request.getName());
+        user.setEmail(request.getEmail());
+
+        User savedUser = userService.createUser(user);
+        return new UserDTO.UserResponse(savedUser);
+    }
+
+    @GetMapping
+    public List<UserDTO.UserResponse> getUsers() {
+        return userService.getAllUsers()
+                // List<Users> => Stream<User>
+                .stream()
+                // User -> UserDTO.UserResponse
+                .map(user -> new UserDTO.UserResponse(user))
+                // .map(UserDTO.UserResponse::new)
+                .toList();
+    }
+
+    @GetMapping("/{id}")
+    public UserDTO.UserResponse getUserById(@PathVariable Long id) {
+        User existUser = userService.getUserById(id);
+        return new UserDTO.UserResponse(existUser);
+    }
+
+    @GetMapping("/email/{email}")
+    public UserDTO.UserResponse getUserByEmail(@PathVariable String email) {
+        return new UserDTO.UserResponse((userService.getUserByEmail(email)));
+    }
+
+
+}
