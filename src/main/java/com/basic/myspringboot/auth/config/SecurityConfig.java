ca493c56e430ef91b03db272bf558b9980a9018a
nahye <knh8783@gmail.com>
2025-05-12 13:29:52 +0900

    add auth

---
+++ src/main/java/com/basic/myspringboot/auth/config/SecurityConfig.java
@@ -0,0 +1,53 @@
+package com.basic.myspringboot.auth.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http.csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> {
+                    // api/users/welcome 패스는 인증없이 접근 가능한 경우
+                    auth.requestMatchers("/users/welcome").permitAll()
+                            // api/users/** 패스는 인증 후에만 접근 가능
+                            .requestMatchers("/users/**").authenticated();
+
+                })
+                // 스프링이 제공하는 인증 폼을 사용함
+                .formLogin(Customizer.withDefaults())
+                .build();
+
+    }
+
+    @Bean
+    //authentication
+    public UserDetailsService userDetailsService(PasswordEncoder encoder) {
+        UserDetails admin = User.withUsername("adminboot")
+                .password(encoder.encode("pwd1"))
+                .roles("ADMIN")
+                .build();
+        UserDetails user = User.withUsername("userboot")
+                .password(encoder.encode("pwd2"))
+                .roles("USER")
+                .build();
+        return new InMemoryUserDetailsManager(admin, user);
+    }
+}
