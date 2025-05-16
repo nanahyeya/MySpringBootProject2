fc67d6b8c6a7e65ec5fa21971eb936d0c634a851
nahye <knh8783@gmail.com>
2025-05-16 09:54:51 +0900

    init student 1toN

--- src/main/java/com/basic/myspringboot/entity/Student.java
+++ src/main/java/com/basic/myspringboot/entity/Student.java
@@ -3,31 +3,26 @@ package com.basic.myspringboot.entity;
 import jakarta.persistence.*;
 import lombok.*;
 
-//Student
 @Entity
 @Table(name = "students")
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-@Getter
-@Setter
-//Non-Owner(종속) 양방향관계
+@Getter @Setter
 public class Student {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "student_id")
     private Long id;
 
     @Column(nullable = false)
     private String name;
 
-    @Column(unique = true, nullable = false)
+    @Column(nullable = false, unique = true)
     private String studentNumber;
 
-    //1:1 지연로딩
-    //양방향 Student에서 StudentDetail 참조할 수 있도록 FK에 해당하는 필드명 mappedBy에 설정한다.
-    //Student와 StudentDetail 의 라이프사이클이 동일하다.
-    @OneToOne(fetch = FetchType.LAZY, mappedBy = "student", cascade = CascadeType.ALL)
+    @OneToOne(mappedBy = "student", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     private StudentDetail studentDetail;
+
+    private Department department;
 }
