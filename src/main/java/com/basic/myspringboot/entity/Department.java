9027c771e9e58179d60e88ac4fe1b5dd401e39bd
nahye <knh8783@gmail.com>
2025-05-16 10:09:20 +0900

    mdify to qtoN

--- src/main/java/com/basic/myspringboot/entity/Department.java
+++ src/main/java/com/basic/myspringboot/entity/Department.java
@@ -14,26 +14,32 @@ import java.util.List;
 @Getter
 @Setter
 public class Department {
-    
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "department_id")
     private Long id;
-    
+
     @Column(nullable = false)
     private String name;
-    
+
     @Column(unique = true, nullable = false)
     private String code;
-    
+
+    //양방향에서 Department에서 Student를 참조할 수 있도록 FK에 해당하는 필드명 mappedBy에 설정한다.
+    @OneToMany(
+            mappedBy = "department",
+            cascade = CascadeType.ALL,
+            fetch = FetchType.LAZY
+    )
     private List<Student> students = new ArrayList<>();
-    
+
     // Helper methods
     public void addStudent(Student student) {
         students.add(student);
         student.setDepartment(this);
     }
-    
+
     public void removeStudent(Student student) {
         students.remove(student);
         student.setDepartment(null);
