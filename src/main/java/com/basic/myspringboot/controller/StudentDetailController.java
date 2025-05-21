d3340878cc7347edbfe6045586e11f97f79669a0
nahye <knh8783@gmail.com>
2025-05-21 14:49:56 +0900

    update

--- src/main/java/com/basic/myspringboot/controller/StudentDetailController.java
+++ src/main/java/com/basic/myspringboot/controller/StudentDetailController.java
@@ -1,7 +1,7 @@
 package com.basic.myspringboot.controller;
 
 import com.basic.myspringboot.controller.dto.StudentDTO;
-import com.basic.myspringboot.service.StudentService;
+import com.basic.myspringboot.service.StudentDetailService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -10,12 +10,13 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+//StudentController 클래스
 @RestController
-@RequestMapping("/api/students")
+@RequestMapping("/api/students/detail")
 @RequiredArgsConstructor
-public class StudentController {
+public class StudentDetailController {
 
-    private final StudentService studentService;
+    private final StudentDetailService studentService;
 
     @GetMapping
     public ResponseEntity<List<StudentDTO.Response>> getAllStudents() {
@@ -38,6 +39,7 @@ public class StudentController {
     @PostMapping
     public ResponseEntity<StudentDTO.Response> createStudent(@Valid @RequestBody StudentDTO.Request request) {
         StudentDTO.Response createdStudent = studentService.createStudent(request);
+        //HttpStatus.CREATED - 201
         return new ResponseEntity<>(createdStudent, HttpStatus.CREATED);
     }
 
